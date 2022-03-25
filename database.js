12a85671346446ef716929f073ceeb7307de34e1
Manuel-ARP <84135386+Manuel-ARP@users.noreply.github.com>
2022-03-25 01:24:25 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-    host: "localhost",
-    user: "root",
-    password: "pepito123",
+    host: "us-cdbr-east-05.cleardb.net",
+    user: "b5fe92b56b576c",
+    password: "04082b6d",
     port: 3306,
-    database: "adoption_system",
-    secret_key: "abcdefg1234"
+    database: "heroku_ffaf949e10e8dad",
+    secret_key: "Viejo_lerdo"
 };
 exports.config = config;
